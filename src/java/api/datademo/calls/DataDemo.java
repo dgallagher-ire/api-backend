47f20c6d32756251f058d0adcf8899e5af7b7164
dgallagher-ire <mobile2158857@gmail.com>
2016-12-25 23:11:59 +0000

    use raw data binding

--- src/java/api/datademo/calls/DataDemo.java
+++ src/java/api/datademo/calls/DataDemo.java
@@ -3,7 +3,9 @@ package api.datademo.calls;
 import api.datademo.APICall;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -24,18 +26,24 @@ public class DataDemo implements APICall {
     @Override
     public final String getOutput() {
         try {
-            final List<Data> list = new ArrayList<>();
-            final Data d1 = new Data();
-            d1.setId(0);
-            d1.setBucket("declan2");
-            d1.setLive(true);
-            d1.setFiles(2222);
-            d1.setBatch(33);
-            d1.setRedShift("mytable");
-            list.add(d1);
-            final DataList dl = new DataList();
-            dl.setList(list);
-            return mapper.writeValueAsString(dl);
+            final List<Map<String,Object>> json = new ArrayList<>();
+            final Map<String, Object> d1 = new HashMap<>();
+            d1.put("id", 0);
+            d1.put("bucket", "declan2");
+            d1.put("live", true);
+            d1.put("files", 2222);
+            d1.put("batch", 33);
+            d1.put("redShift", "mytable");
+            json.add(d1);
+            final Map<String, Object> d2 = new HashMap<>();
+            d2.put("id", 1);
+            d2.put("bucket", "paul");
+            d2.put("live", false);
+            d2.put("files", 3333);
+            d2.put("batch", 44);
+            d2.put("redShift", "rows");
+            json.add(d2);
+            return mapper.writeValueAsString(json);
         } catch (final JsonProcessingException e) {
             return e.toString();
         }
